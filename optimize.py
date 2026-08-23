efba3e39093b691dc60c9e3f1b0c1727392acc52
Ignacio Ramirez Paulino <nacho@fing.edu.uy>
2026-08-23 19:29:41 -0300

    wacamole

---
+++ optimize.py
@@ -0,0 +1,9 @@
+"""
+  paper https://arxiv.org/html/2604.01732v1
+  codigo https://github.com/cutting-stock/csp.git 
+
+   Aparentemente lo mejor que puedo usar es Symmetry Breaking (SB) y Rotation (R) CSP, 
+  'CSP_R_SB.py'; en realidad es codigo experimental, pero se puede extraer lo ppal 
+  sin mucho trabajo. Hay algunas dependencias. Puede valer la pena para 
+  no tener que correr todo en Barraca Paraná una y otra vez.
+ """
